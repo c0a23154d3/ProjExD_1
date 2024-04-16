6ba5953f57f465a4cb946b8e905b40fdf7e858b3
C0A23154 <c0a23154d3@edu.teu.ac.jp>
2024-04-16 18:33:21 +0900

    演習2

---
+++ flying_kokaton.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False) #練習7.1
+    kk_img = pg.image.load("fig/3.png") #練習2
+    kk_img = pg.transform.flip(kk_img,True,False)
+    kk_rct = kk_img.get_rect() #練習8.1
+    kk_rct.center = 300,200 #練習8.2
+    tmr = 0
+
+
+    while True:
+        z = -1
+        y = 0
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            
+            y = -1
+        if key_lst[pg.K_DOWN]:
+            
+            y = 1
+        if key_lst[pg.K_LEFT]:
+            z = -1
+            
+        if key_lst[pg.K_RIGHT]:
+            z = 2
+
+        kk_rct.move_ip([z,y])    
+        x = tmr%3200
+        screen.blit(bg_img, [-x, 0]) #練習6
+        screen.blit(bg_img2,[-x+1600,0]) #練習7.1
+        screen.blit(bg_img, [-x+3200, 0]) #練習7.2
+        screen.blit(bg_img2,[-x+4800,0]) #練習7.2
+
+        screen.blit(kk_img,kk_rct) #練習4 #練習8
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200) #練習5
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
